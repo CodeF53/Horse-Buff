ffc9d3f0b890a4632f6b7cac7ba0a230d74c760d
CodeF53 <fseusb@gmail.com>
2021-11-12 16:38:53 -0700

    Add Icon, Finish Mining Speed, Light Optimization
    Close #1 #2 #3

--- src/main/java/net/F53/HorseBuff/mixin/BreakSpeed.java
+++ src/main/java/net/F53/HorseBuff/mixin/BreakSpeed.java
@@ -1,8 +1,11 @@
 package net.F53.HorseBuff.mixin;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -11,13 +14,15 @@ import net.minecraft.entity.passive.HorseBaseEntity;
 
 // Disable breakspeed debuff
 @Mixin(PlayerEntity.class)
-public class BreakSpeed {
-    /*public ServerPlayerEntity player;
+public abstract class BreakSpeed extends LivingEntity {
+    protected BreakSpeed(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @ModifyConstant(method = "getBlockBreakingSpeed(Lnet/minecraft/block/BlockState;)F", constant = @Constant(floatValue = 5.0F))
     private float HorseBreakSpeed(float speedMultiplier){
-        if (this.player.getRootVehicle() instanceof HorseBaseEntity)
+        if (this.getRootVehicle() instanceof HorseBaseEntity)
             return 1.0F;
         return speedMultiplier;
-    }*/
+    }
 }
--- src/main/java/net/F53/HorseBuff/mixin/NoWander.java
+++ src/main/java/net/F53/HorseBuff/mixin/NoWander.java
@@ -1,9 +1,11 @@
 package net.F53.HorseBuff.mixin;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import net.minecraft.entity.passive.HorseBaseEntity;
@@ -13,9 +15,9 @@ import net.minecraft.entity.passive.HorseBaseEntity;
 public abstract class NoWander {
     @Shadow public abstract boolean isSaddled();
 
-    @ModifyArgs(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/AnimalEntity;travel(Lnet/minecraft/util/math/Vec3d;)V", ordinal = 0))
+    @ModifyArg(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/AnimalEntity;travel(Lnet/minecraft/util/math/Vec3d;)V", ordinal = 0))
     private void lowerWanderSpeed(Args args) {
         if (isSaddled())
-            args.set(0,Vec3d.ZERO);
+            args.set(0, Vec3d.ZERO);
     }
 }
