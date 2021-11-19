336938852457f38b0739c608c3a1affc65642720
CodeF53 <fseusb@gmail.com>
2021-11-19 13:52:41 -0700

    Fix config not being saved

--- src/main/java/net/F53/HorseBuff/config/ModConfig.java
+++ src/main/java/net/F53/HorseBuff/config/ModConfig.java
@@ -1,5 +1,7 @@
 package net.F53.HorseBuff.config;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.F53.HorseBuff.ModInfo;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.*;
@@ -12,8 +14,16 @@ public class ModConfig implements ConfigData{
     //    Breeding Changes
 
     @ConfigEntry.Gui.Tooltip
-    public static boolean noWander = true;
+    public boolean noWander = true;
 
     @ConfigEntry.Gui.Tooltip
-    public static boolean fairBreeds = true;
+    public boolean fairBreeds = true;
+
+    public static void init() {
+        AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
+    }
+
+    public static ModConfig getInstance() {
+        return AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
 }
