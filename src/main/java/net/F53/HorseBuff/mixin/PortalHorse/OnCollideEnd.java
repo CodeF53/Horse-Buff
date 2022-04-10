dceda136e195de792dc19a2f80f8e9ba8183bd7c
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-04-09 20:17:51 -0600

    Disable End Travel in preparation for pull to main

--- src/main/java/net/F53/HorseBuff/mixin/PortalHorse/OnCollideEnd.java
+++ src/main/java/net/F53/HorseBuff/mixin/PortalHorse/OnCollideEnd.java
@@ -4,18 +4,30 @@ import net.F53.HorseBuff.config.ModConfig;
 import net.minecraft.block.EndPortalBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MovementType;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.*;
+import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProperties;
+import net.minecraft.world.biome.source.BiomeAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.F53.HorseBuff.HorseBuffInit;
 
+import java.util.Iterator;
+
 @Mixin(EndPortalBlock.class)
 public class OnCollideEnd {
+    /* disabled until player is moved to horse
+
     // Allow entities with passengers
     // Has to be vehicle bringing player, otherwise you enter end with a burning horse
     @Redirect(method = "onEntityCollision(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V", at = @At(value = "INVOKE", target = "net/minecraft/entity/Entity.hasPassengers ()Z"))
@@ -36,26 +48,26 @@ public class OnCollideEnd {
             // Split vehicle and player
             player.detach();
 
-            // Change player Dim
-            player = player.moveToWorld(destination);
-            assert player != null;
-            player.unsetRemoved();
+
 
             // Change vehicle Dim
             vehicle = vehicle.moveToWorld(destination);
             assert vehicle != null;
             vehicle.unsetRemoved();
 
-            // Drag player over to vehicle
-            if (destination.getRegistryKey() == World.OVERWORLD)
-                HorseBuffInit.LOGGER.info("attempting to drag player over to horse");
-                player.setPosition(vehicle.getPos());
-                player.setPos(vehicle.getX(),vehicle.getY(),vehicle.getZ());
-                ((ServerPlayerEntity) player).teleport(destination, vehicle.getX(),vehicle.getY(),vehicle.getZ(), player.getYaw(), player.getPitch());
-                player.refreshPositionAfterTeleport(vehicle.getPos());
-                player.refreshPositionAndAngles(vehicle.getX(),vehicle.getY(),vehicle.getZ(), player.getYaw(), player.getPitch());
-                ((ServerPlayerEntity) player).networkHandler.requestTeleport(vehicle.getX(),vehicle.getY(),vehicle.getZ(), player.getYaw(), player.getPitch());
-                //TODO: why does player not go?
+            // Change player Dim
+            if (destination.getRegistryKey() == World.OVERWORLD) {
+                // Cant use vanilla moveToWorld because it doesn't let us setpos afterwards
+                this.inTeleportationState = true;
+                ServerWorld serverWorld = this.getWorld();
+                RegistryKey<World> registryKey = serverWorld.getRegistryKey();
+                this.getWorld().removePlayer(this, Entity.RemovalReason.CHANGED_DIMENSION);
+                this.networkHandler.sendPacket(new GameStateChangeS2CPacket(GameStateChangeS2CPacket.GAME_WON, this.seenCredits ? 0.0F : 1.0F));
+                this.seenCredits = true;
+            } else {
+                player.moveToWorld(destination);
+                player.unsetRemoved();
+            }
 
             // Make player remount Vehicle
             player.startRiding(vehicle, true);
@@ -64,4 +76,6 @@ public class OnCollideEnd {
         }
         return vehicle.moveToWorld(destination);
     }
+
+     */
 }
