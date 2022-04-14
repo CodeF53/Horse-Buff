06dd3997421f00ac4b82121c64a5e633e31f16dc
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-04-14 12:56:05 -0600

    Remove Tutorial Comments, fix java version

--- src/main/java/net/F53/HorseBuff/HorseBuffInit.java
+++ src/main/java/net/F53/HorseBuff/HorseBuffInit.java
@@ -8,17 +8,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class HorseBuffInit implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("HorseBuff");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Horse Buff Initialized");
 		ModConfig.init();
 	}
