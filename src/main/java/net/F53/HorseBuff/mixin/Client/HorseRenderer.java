83c4fb3fa8d7b74f011608abbc22c6c849406cc1
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2022-04-18 12:26:58 -0600

    Fix #18

--- src/main/java/net/F53/HorseBuff/mixin/Client/HorseRenderer.java
+++ src/main/java/net/F53/HorseBuff/mixin/Client/HorseRenderer.java
@@ -1,6 +1,5 @@
 package net.F53.HorseBuff.mixin.Client;
 
-import net.F53.HorseBuff.HorseBuffInit;
 import net.F53.HorseBuff.config.ModConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -8,10 +7,13 @@ import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +22,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
+import java.util.List;
 
 import static net.F53.HorseBuff.HorseBuffInit.isJeb;
 
@@ -27,6 +30,10 @@ import static net.F53.HorseBuff.HorseBuffInit.isJeb;
 public abstract class HorseRenderer<T extends LivingEntity, M extends EntityModel<T>> {
     @Shadow
     protected M model;
+
+    @Final
+    @Shadow
+    protected List<FeatureRenderer<T, M>> features;
 
     @Shadow
     protected abstract boolean isVisible(T entity);
@@ -37,10 +44,15 @@ public abstract class HorseRenderer<T extends LivingEntity, M extends EntityMode
     @Shadow
     protected abstract RenderLayer getRenderLayer(T entity, boolean showBody, boolean translucent, boolean showOutline);
 
+
+    @Shadow protected abstract float getAnimationProgress(T entity, float tickDelta);
+
     @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
             at = @At(value = "INVOKE", target = "net/minecraft/client/render/entity/LivingEntityRenderer.isVisible (Lnet/minecraft/entity/LivingEntity;)Z", shift = At.Shift.AFTER))
     void pitchFade(T livingEntity, float f, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, CallbackInfo ci) {
         if (livingEntity instanceof HorseBaseEntity && isVisible(livingEntity)) {
+            boolean shouldRender = false;
+
             float r = 1;
             float g = 1;
             float b = 1;
@@ -51,6 +63,7 @@ public abstract class HorseRenderer<T extends LivingEntity, M extends EntityMode
             RenderLayer renderLayer = getRenderLayer(livingEntity, false, true, false);
 
             if (livingEntity.hasPassenger(MinecraftClient.getInstance().player) && ModConfig.getInstance().pitchFade.enabled) {
+                shouldRender = true;
                 //As a player looks down, opacity of horse decreases.
                 //100% at fadeStartAngle degrees
                 //10% at fadeEndAngle+ degrees
@@ -62,16 +75,36 @@ public abstract class HorseRenderer<T extends LivingEntity, M extends EntityMode
                 ClientPlayerEntity player = MinecraftClient.getInstance().player;
                 assert player != null;
                 opacity = (Math.max(Math.min(100, rate * (player.renderPitch - fadeEndAngle)), minOpacity)) / 100;
-            } if (isJeb(livingEntity)) {
+            }
+            if (isJeb(livingEntity)) {
+                shouldRender = true;
                 float hueOffset = (livingEntity.getUuid().hashCode()%5000)/5000f + (System.currentTimeMillis()%5000)/5000f;
                 Color color = new Color(Color.HSBtoRGB(hueOffset, 0.8f, 1));
                 r = color.getRed()/255f;
                 g = color.getGreen()/255f;
                 b = color.getBlue()/255f;
             }
-            VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(renderLayer);
-            // for some reason opacity with this technique doesn't go below 10, I am not writing a new
-            this.model.render(matrixStack, vertexConsumer, light, overlay, r, g, b, opacity);
+            if (shouldRender) {
+                VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(renderLayer);
+                // for some reason opacity with this technique doesn't go below 10
+                this.model.render(matrixStack, vertexConsumer, light, overlay, r, g, b, opacity);
+
+                // we need to render armor again if it has it because z-fighting
+                // at this point we know it's a horse with a mount on it, so a lot of checks from vanilla code can be removed
+                // only do this if we actually have armor (small optimization)
+                if (((HorseBaseEntity) livingEntity).hasArmorInSlot()) {
+                    // calculate limbAngle and limbDistance for ourselves, because despite coming after them being defined, we don't get access to them.
+                    float limbDistance = MathHelper.lerp(g, (livingEntity).lastLimbDistance, livingEntity.limbDistance);
+                    float limbAngle = (livingEntity).limbAngle - livingEntity.limbDistance * (1.0f - tickDelta);
+                    if (limbDistance > 1.0f) { limbDistance = 1.0f; }
+
+                    for (FeatureRenderer<T, M> feature : this.features) {
+                        feature.render(matrixStack, vertexConsumerProvider, light, livingEntity, limbAngle,
+                                limbDistance, tickDelta, getAnimationProgress(livingEntity,tickDelta),
+                                livingEntity.getHeadYaw(), livingEntity.getPitch());
+                    }
+                }
+            }
         }
     }
 
