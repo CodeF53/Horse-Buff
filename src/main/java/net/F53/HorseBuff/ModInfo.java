a2062cd642f723f5df1dc9bd2f22a96e40187775
F53 <fseusb@gmail.com>
2022-12-11 21:06:52 -0700

    bump version

--- src/main/java/net/F53/HorseBuff/ModInfo.java
+++ src/main/java/net/F53/HorseBuff/ModInfo.java
@@ -4,5 +4,5 @@ package net.F53.HorseBuff;
 
 public final class ModInfo {
     public static final String MODID = "HorseBuff";
-    public static final double VERSION = 1.2;
+    public static final double VERSION = 2.02;
 }
