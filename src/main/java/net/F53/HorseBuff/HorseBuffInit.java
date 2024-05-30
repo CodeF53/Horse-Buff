07b81ca4cb5f2b6aa1075098f03fa2b50b3abdcc
F53 <fseusb@gmail.com>
2024-05-29 21:25:49 -0600

    WIP 1.21 update: remove PortalHorse code

--- src/main/java/net/F53/HorseBuff/HorseBuffInit.java
+++ src/main/java/net/F53/HorseBuff/HorseBuffInit.java
@@ -1,27 +1,16 @@
 package net.F53.HorseBuff;
 
-import net.F53.HorseBuff.utils.TickScheduler;
-import net.fabricmc.api.ModInitializer;
-
 import net.F53.HorseBuff.config.ModConfig;
-
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.UUID;
-
 public class HorseBuffInit implements ModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger("HorseBuff");
+    public static final Logger LOGGER = LogManager.getLogger("HorseBuff");
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Horse Buff Initialized");
-		ModConfig.init();
-		TickScheduler.initialize();
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Horse Buff Initialized");
+        ModConfig.init();
+    }
 }
